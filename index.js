8984d552f39dd4868d491033b477f4de811b229b
Sarah Eis <Korutae4@gmail.com>
2023-09-21 11:57:54 -0700

    progress

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/movies_db');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,12 +16,64 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.get('/api/movies', async(req, res, next) => {
+  try {
+    const SQL = `
+      SELECT * FROM movies
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch(error){
+    next(error);
+  }
+})
+
+app.get('api/movies/:id', async (req, res, next) => {
+  try {
+    const SQL = `SELECT * FROM movies WHERE id=$1;`;
+    const response = await client.query(SQL, [req.params.id]);
+
+    if(!response.rows.length) {
+      next({
+        name:"MissingIDError",
+        message: `movie with id ${req.params.id} not found`,
+      });
+    }
+    res.send(response.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put('api/movies/:id', async(req, res, next) => {
+  try {
+    const SQL = `
+      UPDATE movies
+      SET name = $1, stars = $2
+      WHERE id = $3
+      RETURNING *
+    `;
+    const response = await client.query(SQL, [req.body.name, req.body.stars]);
+  } catch(error) {
+    next(error)
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+      DROP TABLE IF EXISTS movies;
+      CREATE TABLE movies(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        stars INTEGER
+      );
+      INSERT INTO movies (name, stars) VALUES ('New Movie', 3);
+      INSERT INTO movies (name, stars) VALUES ('Older Movie', 5);
+      INSERT INTO movies (name, stars) VALUES ('Bad Movie', 1);
   `;
+  await client.query(SQL);
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
